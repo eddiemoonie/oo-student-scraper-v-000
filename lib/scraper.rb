2049b509576f4379f7cacc89090125b8ff917e98
Eddie Moon <eddiemoonie@gmail.com>
2018-03-23 19:19:50 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,25 +4,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    #index_page = Nokogiri::HTML(open(index_url))
-    #students = []
-    #index_page.css("div.roster-cards-container").each do |roster_card|
-      #roster_card.css(".student-card a").each do |student|
-        #student_name = student.css(".student-name").text
-        #student_location = student.css(".student-location").text
-        #student_prof_link = "#{student.attr('href')}"
-        #students << {name: student_name, location: student_location, profile_url: student_prof_link}
-      #end
-    #end
-    #students
     index_page = Nokogiri::HTML(open(index_url))
     students = []
-    index_page.css("div.roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
-        student_profile_link = "#{student.attr('href')}"
-        student_location = student.css('.student-location').text
-        student_name = student.css('.student-name').text
-        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+    index_page.css("div.roster-cards-container").each do |roster_card|
+      roster_card.css(".student-card a").each do |student|
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
+        student_prof_link = "#{student.attr('href')}"
+        students << {name: student_name, location: student_location, profile_url: student_prof_link}
       end
     end
     students
