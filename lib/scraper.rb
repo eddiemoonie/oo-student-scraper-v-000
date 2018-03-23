491057687ce585a99b465e49dd568487ae3ec13b
Eddie Moon <eddiemoonie@gmail.com>
2018-03-23 19:11:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,9 @@ class Scraper
     students = []
     index_page.css("div.roster_cards_container").each do |roster_card|
       roster_card.css(".student_card a").each do |student|
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
+        student_prof_link = "#{student.attr('href')}"
   end
 
   def self.scrape_profile_page(profile_url)
