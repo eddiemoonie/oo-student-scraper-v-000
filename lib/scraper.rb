33cb7654c2e46751c17a12960216138f4376f0c1
Eddie Moon <eddiemoonie@gmail.com>
2018-03-23 19:05:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     students = []
-    index_page.css(div.roster_card_container).each do |student|
-
+    index_page.css("div.roster_cards_container").each do |roster_card|
+      roster_card.css(".student_card a").each do |student|
   end
 
   def self.scrape_profile_page(profile_url)
